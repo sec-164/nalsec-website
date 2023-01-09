1727d9162ca329d8643fc48074e8c98c0dd6d92a
yokoiwa <admin@yokoiwahiroki.name>
2023-01-09 17:43:19 +0900

    fixed basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   images: {
     loader: "custom",
   },
+  trailingSlash: true,
+  basePath: isProduction ? "/nalsec-website" : "",
+  assetPrefix: isProduction ? "/nalsec-website" : "",
   experimental: {
     // appDir: true
   },
